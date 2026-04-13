3e5f329baae8f7a6d8274ac2c87d06c75c2d7f3c
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-13 14:56:07 -0300

    Create _atividade_4.java

---
+++ _atividade_4.java
@@ -0,0 +1,22 @@
+package Java_Lista_3_Ternario;
+
+import java.util.Scanner;
+
+public class _atividade_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int idade;
+
+        System.out.println("Qual é a sua idade?");
+        idade = sc.nextInt();
+
+        while (idade == 0) {
+            System.out.println("Idade inválida, digite outra.");
+            idade = sc.nextInt();
+        }
+
+        String pessoa = (idade >= 18) ? "Você é maior de idade" : "Você é menor de idade";
+
+        System.out.println(pessoa);
+    }
+}
